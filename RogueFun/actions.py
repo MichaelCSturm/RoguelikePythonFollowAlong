e2f5e432ad4fd5641b0087d3790e1bc65f5caa19
Not_Shultz <59833741+MichaelCSturm@users.noreply.github.com>
2024-02-09 14:55:12 -0600

    Updates to all files!

--- RogueFun/actions.py
+++ RogueFun/actions.py
@@ -25,11 +25,11 @@ class MovementAction(Action):
 
         self.dx = dx
         self.dy = dy
-def perform(self, engine: Engine, entity: Entity) -> None:
-     dest_x = entity.x + self.dx
-     dest_y = entity.y + self.dy
-     if not engine.game_map.in_bounds(dest_x, dest_y):
-         return  # Destination is out of bounds.
-     if not engine.game_map.tiles["walkable"][dest_x, dest_y]:
-         return  # Destination is blocked by a tile.
-     entity.move(self.dx, self.dy)
+    def perform(self, engine: Engine, entity: Entity) -> None:
+        dest_x = entity.x + self.dx
+        dest_y = entity.y + self.dy
+        if not engine.game_map.in_bounds(dest_x, dest_y):
+            return  # Destination is out of bounds.
+        if not engine.game_map.tiles["walkable"][dest_x, dest_y]:
+            return  # Destination is blocked by a tile.
+        entity.move(self.dx, self.dy)
